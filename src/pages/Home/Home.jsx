66e220cb182185242558a2a4c41843eaa90165f3
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-12-05 20:28:45 +0100

    home with animation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,53 @@
-import React,{useState, useEffect} from 'react';
-import './Home.scss'
-import  logo  from '../../assets/images/logoFavorit.jpeg';
-
+import React, { useState, useEffect } from "react";
+import "./Home.scss";
+import logo from "../../assets/images/logoFavorit.jpeg";
 
 const Home = () => {
-  
-
   return (
-   <>
-    <div className='home'>
-    <img className="logo" src={logo} alt="logo"></img>
-    <p>Somos un gastrobar innovador, ofrecemos los mejores servicios para todo tipo de clientes. Cocina oriental, occidental y tradicional con cartas para vegetarianos y celíacos (platos sin gluten), también puedes disfrutar de nuestro delicioso café, vino y copas con tus familiares y amigos.</p>
-          {/* <img className="home__image" src={fondo} alt="logo"></img> */}
-    </div>
-   
-   </>
-  )
-}
+    <>
+      <div className="home">
+        {/* <img className="logo" src={logo} alt="logo"></img>
+        <p>
+          Somos un gastrobar innovador, ofrecemos los mejores servicios para
+          todo tipo de clientes. Cocina oriental, occidental y tradicional con
+          cartas para vegetarianos y celíacos (platos sin gluten), también
+          puedes disfrutar de nuestro delicioso café, vino y copas con tus
+          familiares y amigos.
+        </p> */}
+        <div class="star-wars-intro">
+        <h2 class="main-logo">
+           
+          </h2>
+          <p class="intro-text"></p>
 
-export default Home
+         
+
+          <div class="main-content">
+            <div class="title-content">
+            
+              <p class="content-header">
+              <img className="logo" src={logo} alt="logo"></img>
+              </p>
+             
+
+              <p class="content-body">
+                Somos un gastrobar innovador, ofrecemos los mejores servicios
+                para todo tipo de clientes. Cocina oriental, occidental y
+                tradicional con cartas para vegetarianos y celíacos, también puedes disfrutar de nuestro delicioso café,
+                vino y copas con tus familiares y amigos.
+                {/* <br/>
+                <img className="logo" src={logo} alt="logo"></img> */}
+              </p>
+              {/* <a href="./StarScroll.zip" class="space-button">Download The Code Now!</a> */}
+             
+            </div>
+            
+          </div>
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
