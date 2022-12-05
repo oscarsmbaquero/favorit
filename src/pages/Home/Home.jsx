baf0d2b1b0b12a02ffc8253a21f4ae538122057f
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-12-05 21:35:54 +0100

    delete animation at home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,15 +6,15 @@ const Home = () => {
   return (
     <>
       <div className="home">
-        {/* <img className="logo" src={logo} alt="logo"></img>
+        <img className="logo" src={logo} alt="logo"></img>
         <p>
           Somos un gastrobar innovador, ofrecemos los mejores servicios para
           todo tipo de clientes. Cocina oriental, occidental y tradicional con
-          cartas para vegetarianos y celíacos (platos sin gluten), también
+          cartas para vegetarianos y celíacos. También
           puedes disfrutar de nuestro delicioso café, vino y copas con tus
           familiares y amigos.
-        </p> */}
-        <div class="star-wars-intro">
+        </p>
+        {/* <div class="star-wars-intro">
           <h2 class="main-logo"></h2>
           <p class="intro-text"></p>
 
@@ -31,13 +31,12 @@ const Home = () => {
                 También
                 puedes disfrutar de nuestro delicioso café, vino y copas con tus
                 familiares y amigos.
-                {/* <br/>
-                <img className="logo" src={logo} alt="logo"></img> */}
+               
               </p>
-              {/* <a href="./StarScroll.zip" class="space-button">Download The Code Now!</a> */}
+              
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
