c624f953ca8fe53495f8bd5f1fa239b719c41e37
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-12-05 20:33:39 +0100

    fixed home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,35 +15,28 @@ const Home = () => {
           familiares y amigos.
         </p> */}
         <div class="star-wars-intro">
-        <h2 class="main-logo">
-           
-          </h2>
+          <h2 class="main-logo"></h2>
           <p class="intro-text"></p>
-
-         
 
           <div class="main-content">
             <div class="title-content">
-            
               <p class="content-header">
-              <img className="logo" src={logo} alt="logo"></img>
+                <img className="logo" src={logo} alt="logo"></img>
               </p>
-             
 
               <p class="content-body">
                 Somos un gastrobar innovador, ofrecemos los mejores servicios
                 para todo tipo de clientes. Cocina oriental, occidental y
-                tradicional con cartas para vegetarianos y celíacos, también puedes disfrutar de nuestro delicioso café,
-                vino y copas con tus familiares y amigos.
+                tradicional con cartas para vegetarianos y celíacos.
+                También
+                puedes disfrutar de nuestro delicioso café, vino y copas con tus
+                familiares y amigos.
                 {/* <br/>
                 <img className="logo" src={logo} alt="logo"></img> */}
               </p>
               {/* <a href="./StarScroll.zip" class="space-button">Download The Code Now!</a> */}
-             
             </div>
-            
           </div>
-         
         </div>
       </div>
     </>
