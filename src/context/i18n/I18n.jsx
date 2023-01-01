0858b9f23c85c46882e814d62febfc37a5864f58
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-01-01 20:03:34 +0100

    favicon.ico change

--- src/context/i18n/I18n.jsx
+++ src/context/i18n/I18n.jsx
@@ -4,20 +4,18 @@ import { ContextI18n } from "../../App";
 import "./I18n.scss";
 import spain from "../../assets/banderas/spain.png";
 import english from "../../assets/banderas/english.jpg";
-import portuguese from "../../assets/banderas/portugal.jpg";
-import ikurriña from "../../assets/banderas/ikurriña.jpg";
-import french from "../../assets/banderas/french.png";
 //import { makeStyles } from "@material-ui/core/styles";
 
 
 
 const I18n = () => {
   const { locale, selectLanguage } = useContext(ContextI18n);
+  console.log(locale);
   return (
     <div class="i18n">
       {locale === "es-ES" ? (
         <IconButton onClick={() => selectLanguage("en")}>
-        <img src={english} alt="spain" className="bandera"></img>
+        <img src={english} alt="english" className="bandera"></img>
           {/* <Avatar
             sx={{ width: 24, height: 24 }}
             src={english}
